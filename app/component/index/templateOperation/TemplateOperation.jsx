bc81668a24eaf113763d5cb8165351bb77985a55
gloomysw <shaow@axon.com.cn>
2018-09-03 16:19:46 +0800

    修正头部背景色,增加模板操作公共组件

--- app/component/index/templateOperation/TemplateOperation.jsx
+++ app/component/index/templateOperation/TemplateOperation.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import '../../../public/css/templateOperation/templateOperation.pcss'
+import style from '../../../public/css/templateOperation/templateOperation.pcss.json'
 
 class TemplateOperation extends React.Component{
     constructor(props) {
