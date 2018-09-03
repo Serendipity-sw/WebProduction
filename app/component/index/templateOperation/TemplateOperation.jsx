12cd64471e6cfc8d9d81ef715f5e9ab5a32d3d77
gloomysw <shaow@axon.com.cn>
2018-09-03 17:56:37 +0800

    增加左侧模板菜单空组件

--- app/component/index/templateOperation/TemplateOperation.jsx
+++ app/component/index/templateOperation/TemplateOperation.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import '../../../public/css/templateOperation/templateOperation.pcss'
 import style from '../../../public/css/templateOperation/templateOperation.pcss.json'
+import TemplateMenu from "./templateMenu/TemplateMenu";
 
-class TemplateOperation extends React.Component{
+class TemplateOperation extends React.Component {
     constructor(props) {
         super(props);
 
@@ -11,7 +12,7 @@ class TemplateOperation extends React.Component{
     render() {
         return (
             <div className={style.templateOperation}>
-
+                <TemplateMenu/>
             </div>
         );
     }
---
+++ app/component/index/templateOperation/templateMenu/TemplateMenu.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import '../../../../public/css/templateOperation/templateMenu/templateMenu.pcss'
+import style from '../../../../public/css/templateOperation/templateMenu/templateMenu.pcss.json'
+
+class TemplateMenu extends React.Component{
+    constructor(props) {
+        super(props);
+
+    }
+
+    render() {
+        return (
+            <div>
+
+            </div>
+        );
+    }
+}
+
+export default TemplateMenu
