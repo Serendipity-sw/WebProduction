42fddb578b612a55ff503a3e336e246dd6eb0d3c
gloomysw <shaow@axon.com.cn>
2018-09-03 16:23:10 +0800

    模板总区域完成

--- app/component/index/templateOperation/TemplateOperation.jsx
+++ app/component/index/templateOperation/TemplateOperation.jsx
@@ -10,7 +10,7 @@ class TemplateOperation extends React.Component{
 
     render() {
         return (
-            <div>
+            <div className={style.templateOperation}>
 
             </div>
         );
