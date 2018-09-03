0b1d445f3df2fe1a76bdbc6536f7caa0bde0b7ca
gloomysw <shaow@axon.com.cn>
2018-09-03 15:06:24 +0800

    头部样式完成

--- app/component/common/Header.jsx
+++ app/component/common/Header.jsx
@@ -1,16 +1,79 @@
 import React from 'react'
+import '../../public/css/common/head.pcss'
+import iconStyle from '../../public/css/iconFont.pcss.json'
+import style from '../../public/css/common/head.pcss.json'
+import {Button} from "antd";
 
-class Header extends React.Component{
+class Header extends React.Component {
     constructor(props) {
         super(props);
-
+        this.state = {
+            btnAreaList: [
+                {
+                    iconStyle: style.contentIcon,
+                    iconName: 'icon-wenben',
+                    btnName: '文本',
+                },
+                {
+                    iconStyle: style.pictureIcon,
+                    iconName: 'icon-tupian',
+                    btnName: '图片',
+                },
+                {
+                    iconStyle: style.backgroundIcon,
+                    iconName: 'icon-tupian',
+                    btnName: '背景',
+                },
+                {
+                    iconStyle: style.musicIcon,
+                    iconName: 'icon-yinyue',
+                    btnName: '音乐',
+                },
+                {
+                    iconStyle: style.shapeIcon,
+                    iconName: 'icon-xingzhuang',
+                    btnName: '形状',
+                },
+                {
+                    iconStyle: style.componentIcon,
+                    iconName: 'icon-wenben',
+                    btnName: '组件',
+                },
+                {
+                    iconStyle: style.formIcon,
+                    iconName: 'icon-biaodan',
+                    btnName: '表单',
+                },
+                {
+                    iconStyle: style.specialEffectsIcon,
+                    iconName: 'icon-texiao',
+                    btnName: '特效',
+                },
+            ]
+        }
     }
 
     render() {
         return (
-            <div>
-
-            </div>
+            <header>
+                {
+                    this.state.btnAreaList.map((item, index) =>
+                        <div className={style.btnArea} key={index}>
+                            <svg className={[iconStyle.icon, style.icon, style.iconStyle].join(' ')}
+                                 aria-hidden="true">
+                                <use xlinkHref={"#"+item.iconName}></use>
+                            </svg>
+                            <span className={style.content}>{item.btnName}</span>
+                        </div>
+                    )
+                }
+                <div className={style.operatingArea}>
+                    <Button type="primary">预览和设置</Button>
+                    <Button type="primary">保存</Button>
+                    <Button type="primary">发布</Button>
+                    <Button className={style.exitBtn}>退出</Button>
+                </div>
+            </header>
         );
     }
 }
--- app/component/index/Index.jsx
+++ app/component/index/Index.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
+import '../../public/css/cssVariable.pcss'
 import '../../public/css/common.pcss'
+import '../../public/css/iconFont.pcss'
 import style from '../../public/css/common.pcss.json'
-import Header from "../common/Header";
+import Header from "../common/Header"
+
+//加载阿里矢量图图标
+require('../../public/js/iconFont/iconFont')
 
 class Index extends React.Component {
     render() {
