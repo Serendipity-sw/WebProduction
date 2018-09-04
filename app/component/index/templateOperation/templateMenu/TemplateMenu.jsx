d443f3b9458688b85efbdddaa05e015400d99960
gloomysw <shaow@axon.com.cn>
2018-09-04 14:41:38 +0800

    左侧菜单基础功能完成

--- app/component/index/templateOperation/templateMenu/TemplateMenu.jsx
+++ app/component/index/templateOperation/templateMenu/TemplateMenu.jsx
@@ -6,17 +6,24 @@ import IconFont from "../../../common/IconFont";
 class TemplateMenu extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            tabIndex: 0
+        }
+    }
 
+    menuRowsClick=({target})=>{
+        let selectClick = parseInt(target.parentElement.getAttribute('tabIndex'))
+        this.setState({tabIndex:this.state.tabIndex===selectClick?0:selectClick})
     }
 
     render() {
         return (
-            <div className={style.menuArea}>
-                <div className={style.menuRows}>
+            <div className={style.menuArea} onClick={this.menuRowsClick}>
+                <div tabIndex={1}  className={[style.menuRows,this.state.tabIndex===1?style.select:''].join(' ')}>
                     <IconFont classArray={[style.iconStyle]} iconName={'icon-danyemoban'}/>
                     <span className={style.content}>单页模板</span>
                 </div>
-                <div className={style.menuRows}>
+                <div tabIndex={2}  className={[style.menuRows,this.state.tabIndex===2?style.select:''].join(' ')}>
                     <IconFont classArray={[style.iconStyle]} iconName={'icon-fangkuai'}/>
                     <span className={style.content}>功能模板</span>
                 </div>
