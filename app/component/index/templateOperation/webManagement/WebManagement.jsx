a0c5bdf7a0c2f4b7ffe2e5d0761008fc78555070
gloomysw <shaow@axon.com.cn>
2018-09-07 11:16:36 +0800

    页面管理左侧菜单基础骨架及样式完成

--- app/component/index/templateOperation/webManagement/WebManagement.jsx
+++ app/component/index/templateOperation/webManagement/WebManagement.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
 import '../../../../public/css/templateOperation/webManagement/webManagement.pcss'
 import style from '../../../../public/css/templateOperation/webManagement/webManagement.pcss.json'
+import {Tabs} from 'antd'
 
-class WebManagement extends React.Component{
+const TabPane = Tabs.TabPane
+
+class WebManagement extends React.Component {
     constructor(props) {
         super(props);
 
@@ -11,7 +14,10 @@ class WebManagement extends React.Component{
     render() {
         return (
             <div className={style.managementArea}>
-
+                <Tabs defaultActiveKey="1" className={style.tabs}>
+                    <TabPane tab="图层管理" key="1">Content of Tab Pane 1</TabPane>
+                    <TabPane tab="页面管理" key="2">Content of Tab Pane 2</TabPane>
+                </Tabs>
             </div>
         );
     }
